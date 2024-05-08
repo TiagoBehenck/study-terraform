7b87eef92bc3bdeebd9cd32506563297465c6d09
Tiago Behenck <tiago.behenck.santos@gmail.com>
2024-05-08 09:45:31 -0300

    create a new bucket S3 using module

--- main.tf
+++ main.tf
@@ -16,6 +16,20 @@ provider "aws" {
   region = "us-east-1"
 }
 
+module "s3_bucket" {
+  source = "terraform-aws-modules/s3-bucket/aws"
+
+  bucket = "millennium-falcon-its-so-nice-i-tried"
+  acl    = "private"
+
+  control_object_ownership = true
+  object_ownership         = "ObjectWriter"
+
+  versioning = {
+    enabled = true
+  }
+}
+
 module "new-vpc" {
   source = "./modules/vpc"
   prefix = var.prefix
